5cb7ea48f93028c084e387269461eb5967836259
gaoooon <gaon9749@gmail.com>
2025-01-04 03:33:34 +0900

    feat : login 페이지 제작

---
+++ src/pageContainers/login/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import Image from 'next/image';
+
+import { KakaoIcon } from '@/assets';
+import { Header } from '@/components';
+
+import * as S from './style';
+
+const ImageSrc = '/login/main.png';
+
+const LoginPage = () => {
+  return (
+    <>
+      <Header text='BIGBRO' />
+      <S.Container>
+        <S.ImageWrapper>
+          <Image src={ImageSrc} alt='메인 이미지' fill quality={100} />
+        </S.ImageWrapper>
+
+        <S.LoginButton href={`${process.env.NEXT_PUBLIC_KAKAO_LOGIN_URL}`}>
+          <KakaoIcon />
+          <S.LoginText>카카오 계정으로 로그인</S.LoginText>
+        </S.LoginButton>
+      </S.Container>
+    </>
+  );
+};
+
+export default LoginPage;
