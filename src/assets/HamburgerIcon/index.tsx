04cd79b17a17e15b26050511ac009ce947cb34a7
gaoooon <gaon9749@gmail.com>
2025-01-04 03:32:28 +0900

    feat : KakaoIcon, LeftArrowIcon, HamburgerIcon, CartIcon, PeopleIcon asset 추가

---
+++ src/assets/HamburgerIcon/index.tsx
@@ -0,0 +1,15 @@
+const HamburgerIcon = () => (
+  <svg
+    width='1.5rem'
+    height='1.5625rem'
+    viewBox='0 0 24 25'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <rect x='2' y='6.5' width='20' height='1.5' fill='#D9D9D9' />
+    <rect x='2' y='12' width='20' height='1.5' fill='#D9D9D9' />
+    <rect x='2' y='17.5' width='20' height='1.5' fill='#D9D9D9' />
+  </svg>
+);
+
+export default HamburgerIcon;
