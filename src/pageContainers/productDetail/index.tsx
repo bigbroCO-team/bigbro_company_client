df2b9d5cbb9183593dac9eba917f227c339fdcd9
gaoooon <gaon9749@gmail.com>
2025-01-16 21:18:23 +0900

    feat : product detail page 추가

---
+++ src/pageContainers/productDetail/index.tsx
@@ -0,0 +1,168 @@
+'use client';
+
+import Image from 'next/image';
+
+import { MinusIcon, PlusIcon, XIcon } from '@/assets';
+import { Header } from '@/components';
+import { ProductType } from '@/types';
+import { formatKRW } from '@/utils';
+
+import { ChangeEvent, useState } from 'react';
+
+import * as S from './style';
+
+interface ProductDetailPageProps {
+  id: string;
+}
+
+const mockData: ProductType = {
+  id: 4,
+  options: ['Standard', 'Deluxe', 'Pro'],
+  images: [
+    'https://example.com/product4-img1.jpg',
+    'https://example.com/product4-img2.jpg',
+  ],
+  brand: 'GONGNEWGI',
+  name: 'Gongnewgi Smartwatch',
+  description: 'Feature-packed smartwatch for daily use.',
+  price: 199.99,
+  discount: 20,
+  status: 'empty',
+  created: '2024-12-25T12:00:00Z',
+};
+
+interface SelectedOptionType {
+  name: string;
+  count: number;
+}
+
+const ProductDetailPage = ({ id }: ProductDetailPageProps) => {
+  const [mainImage, setMainImage] = useState<string>(mockData.images[0]);
+  const [selectedOption, setSelectedOption] = useState<SelectedOptionType[]>(
+    []
+  );
+  const discountedPrice = (100 - mockData.discount) * 0.01 * mockData.price;
+  const totalPrice =
+    selectedOption.reduce((a, c) => a + c.count, 0) * discountedPrice;
+
+  const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    if (selectedOption.some(({ name }) => name === value)) return;
+
+    setSelectedOption((prev) => [...prev, { name: value, count: 1 }]);
+  };
+
+  const handleXButtonClick = (name: string) => {
+    const deletedOption = selectedOption.filter(
+      (option) => option.name !== name
+    );
+
+    setSelectedOption(deletedOption);
+  };
+
+  const handlePlusButtonClick = (name: string) => {
+    const newOption = selectedOption.map((option) =>
+      option.name === name ? { ...option, count: option.count + 1 } : option
+    );
+
+    setSelectedOption(newOption);
+  };
+
+  const handleMinusButtonClick = (name: string) => {
+    const newOption = selectedOption.map((option) =>
+      option.name === name
+        ? { ...option, count: option.count === 1 ? 1 : option.count - 1 }
+        : option
+    );
+
+    setSelectedOption(newOption);
+  };
+
+  return (
+    <S.Container>
+      <Header text={mockData.brand} backUrl={`/${mockData.brand}`} />
+      <S.ImageBox>
+        <S.MainImageWrapper>
+          <Image src={mainImage} alt={`${mockData.name} 이미지`} fill />
+        </S.MainImageWrapper>
+        <S.ImageController>
+          {mockData.images.map((image) => (
+            <S.SubImageWrapper
+              key={image}
+              isSelected={image === mainImage}
+              onClick={() => setMainImage(image)}
+            >
+              <Image src={image} alt={`${mockData.name} 이미지`} fill />
+            </S.SubImageWrapper>
+          ))}
+        </S.ImageController>
+      </S.ImageBox>
+      <S.InfoBox>
+        <S.Name>{mockData.name}</S.Name>
+        <S.PriceBox>
+          {mockData.discount && (
+            <S.DefaultPrice>{formatKRW(mockData.price)}원</S.DefaultPrice>
+          )}
+          <S.BottomBox>
+            {mockData.discount && (
+              <S.DiscountRate>{mockData.discount}%</S.DiscountRate>
+            )}
+            <S.DiscountedPrice>
+              {formatKRW(discountedPrice)}원
+            </S.DiscountedPrice>
+          </S.BottomBox>
+        </S.PriceBox>
+        <S.Hr />
+        <S.DeliveryInfo>
+          <span>배송</span> <span>로젠택배 (출고 후 2~3일 소요)</span>
+        </S.DeliveryInfo>
+        <S.Hr />
+        <S.SelectBox>
+          상품 선택
+          <S.Select onChange={handleSelectChange}>
+            <option disabled hidden selected>
+              상품을 선택해주세요
+            </option>
+            {mockData.options.map((option) => (
+              <option key={option}>{option}</option>
+            ))}
+          </S.Select>
+          {selectedOption.map(({ name, count }) => (
+            <S.OptionCard key={name}>
+              <S.CardTopBox>
+                {name}
+                <S.XButton onClick={() => handleXButtonClick(name)}>
+                  <XIcon />
+                </S.XButton>
+              </S.CardTopBox>
+              <S.CardBottomBox>
+                <S.CountBox>
+                  <S.ControlButton onClick={() => handleMinusButtonClick(name)}>
+                    <MinusIcon />
+                  </S.ControlButton>
+                  {count}
+                  <S.ControlButton onClick={() => handlePlusButtonClick(name)}>
+                    <PlusIcon />
+                  </S.ControlButton>
+                </S.CountBox>
+                {formatKRW(discountedPrice)}원
+              </S.CardBottomBox>
+            </S.OptionCard>
+          ))}
+        </S.SelectBox>
+        <S.Hr />
+        <S.TotalPriceBox>
+          총 상품 금액:
+          <S.TotalPrice>{formatKRW(totalPrice)}원</S.TotalPrice>
+        </S.TotalPriceBox>
+        <S.ButtonBox>
+          <S.CartButton>장바구니</S.CartButton>
+          <S.BuyButton>구매하기</S.BuyButton>
+        </S.ButtonBox>
+        <S.Description>{mockData.description}</S.Description>
+      </S.InfoBox>
+    </S.Container>
+  );
+};
+
+export default ProductDetailPage;
---
+++ src/pageContainers/productDetail/style.ts
@@ -0,0 +1,257 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  position: relative;
+  padding: 6.5rem 1rem 0 1rem;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  overflow-y: scroll;
+`;
+
+export const ImageBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+`;
+
+export const MainImageWrapper = styled.div`
+  position: relative;
+  width: 35.5rem;
+  height: 35.5rem;
+`;
+
+export const ImageController = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 1rem;
+`;
+
+export const InfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const Hr = styled.div`
+  width: 100%;
+  height: 0.0625rem;
+  background-color: ${({ theme }) => theme.color.gray100};
+  margin: 1.5rem 0;
+`;
+
+export const Name = styled.p`
+  color: ${({ theme }) => theme.color.white};
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.375rem;
+`;
+
+export const PriceBox = styled.div`
+  margin-top: 0.75rem;
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`;
+
+export const BottomBox = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+`;
+
+export const DefaultPrice = styled.p`
+  color: ${({ theme }) => theme.color.gray1};
+  font-size: 2.25rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.9375rem;
+  text-decoration: line-through;
+`;
+
+export const DiscountedPrice = styled.p`
+  color: ${({ theme }) => theme.color.white};
+  font-size: 2.5rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.9375rem;
+`;
+
+export const DiscountRate = styled.p`
+  color: ${({ theme }) => theme.color.error};
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.9375rem;
+`;
+
+export const SelectBox = styled.div`
+  color: ${({ theme }) => theme.color.gray1};
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.75rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`;
+
+export const DeliveryInfo = styled.p`
+  color: ${({ theme }) => theme.color.gray1};
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.75rem;
+  display: flex;
+  gap: 2.5rem;
+`;
+
+export const Select = styled.select`
+  width: 100%;
+  height: 3rem;
+  background-color: ${({ theme }) => theme.color.dark};
+  border: 0.0625rem solid ${({ theme }) => theme.color.gray100};
+  color: #a8a3a3;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.5rem;
+  padding: 0.5rem 1rem;
+`;
+
+export const OptionCard = styled.div`
+  background-color: ${({ theme }) => theme.color.dark};
+  border: 0.0625rem solid ${({ theme }) => theme.color.gray100};
+  display: flex;
+  padding: 1.5rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.5rem;
+`;
+
+export const CardTopBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: ${({ theme }) => theme.color.white};
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.75rem;
+`;
+
+export const CardBottomBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: ${({ theme }) => theme.color.white};
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.375rem;
+`;
+
+export const XButton = styled.button`
+  height: 1.5rem;
+`;
+
+export const CountBox = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.color.white};
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.75rem;
+  padding: 0.25rem 0.5rem;
+  gap: 0.5rem;
+  border-radius: 0.25rem;
+  border: 0.0625rem solid ${({ theme }) => theme.color.gray1};
+`;
+
+export const ControlButton = styled.button`
+  height: 1rem;
+`;
+
+export const TotalPriceBox = styled.div`
+  width: 100%;
+  color: ${({ theme }) => theme.color.white};
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.75rem;
+
+  display: flex;
+  justify-content: end;
+  gap: 1.5rem;
+  align-items: center;
+  margin-top: 1rem;
+`;
+
+export const TotalPrice = styled.p`
+  color: ${({ theme }) => theme.color.white};
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.6875rem;
+`;
+
+export const ButtonBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1.75rem;
+  width: 100%;
+`;
+
+export const BuyButton = styled.button`
+  width: 16.5rem;
+  height: 3.5rem;
+  color: ${({ theme }) => theme.color.dark};
+  background-color: ${({ theme }) => theme.color.main};
+  text-align: center;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.375rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CartButton = styled(BuyButton)`
+  color: ${({ theme }) => theme.color.main};
+  background-color: ${({ theme }) => theme.color.dark};
+  border: 0.0625rem solid ${({ theme }) => theme.color.main};
+`;
+
+export const Description = styled.div`
+  color: ${({ theme }) => theme.color.gray3};
+  margin: 2.5rem 0;
+
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 2rem;
+  width: 100%;
+  white-space: pre-wrap;
+`;
+
+export const SubImageWrapper = styled(MainImageWrapper)<{
+  isSelected: boolean;
+}>`
+  box-sizing: border-box;
+  width: 5rem;
+  height: 5rem;
+  cursor: pointer;
+  border: ${({ isSelected }) => (isSelected ? 0.0625 : 0)}rem solid
+    ${({ theme }) => theme.color.main};
+`;
